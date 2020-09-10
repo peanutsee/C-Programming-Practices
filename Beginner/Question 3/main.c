de5a08290e93b6d73b377e4e0b45862aecf309cb
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-09-10 21:16:09 +0800

    Update main.c

--- Beginner/Question 3/main.c
+++ Beginner/Question 3/main.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-/*
-Write a program that prompts for 2 numbers, and then use loops to calculate and display a list of all prime
-numbers between the 2 numbers. You can assume the user will enter a start number less than end.
-*/
 
 bool prime(n){
     if (n==1){
