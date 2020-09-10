95dd3a3376b215353cb6325543d40ed3f782e7d7
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-09-10 21:15:50 +0800

    Update main.c

--- Beginner/Question 4/main.c
+++ Beginner/Question 4/main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-//Write a program that takes as input a positive integer, and displays the number of digits in that integer. Use
-//ONLY concepts covered so far.
+
 int main(void){
     int counter;
     int user_input;
