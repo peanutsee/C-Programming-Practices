541633065fafc1b4fff6db58da0a9241f1849298
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-09-16 21:27:44 +0800

    Add files via upload

---
+++ Beginner/Question 10/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+/*
+Solve the fibonacci question using recursion
+*/
+int fibonacci(int n){
+    if (n==1) {
+        return 0;
+    } else if (n==2){
+        return 1;
+    } else if (n==3) {
+        return 2;
+    } else {
+        return fibonacci(n-2) + fibonacci(n - 1);
+    }
+}
+
+int main(void){
+  printf("%d", fibonacci(7));
+}
