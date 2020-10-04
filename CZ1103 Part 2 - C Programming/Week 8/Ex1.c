b91cfae2c1da1e75d84709ad8c811f58591c4dcf
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-04 14:13:23 +0800

    Add files via upload

---
+++ CZ1103 Part 2 - C Programming/Week 8/Ex1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void grader(int score){
+    if (score<44){
+        printf("Grade = F\n");
+    } else if (score<54){
+        printf("Grade = D\n");
+    } else if (score<64){
+        printf("Grade = C\n");
+    } else if (score<74){
+        printf("Grade = B\n");
+    } else if (score<=100) {
+        printf("Grade = A\n");
+    } else {
+        printf("INVALID AH SIA\n");
+    }
+}
+
+int main(){
+    while (1){
+        int student_id;
+        printf("Enter Student ID: ");
+        scanf("%d", &student_id);
+        if (student_id != -1){
+            int student_score;
+            printf("Enter Mark: ");
+            scanf("%d", &student_score);
+            grader(student_score);
+        } else {
+            printf("END OF PROGRAMME\n");
+            break;
+        }
+    }
+}
+
