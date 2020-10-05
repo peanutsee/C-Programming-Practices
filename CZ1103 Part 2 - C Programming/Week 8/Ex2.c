c3b46e5a869bae1d0332ff8393aba8a2538ca492
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-05 10:13:29 +0800

    I used a dummy variable to catch the unwanted -1.

--- CZ1103 Part 2 - C Programming/Week 8/Ex2.c
+++ CZ1103 Part 2 - C Programming/Week 8/Ex2.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
-#include <math.h>
 
-double averager(int n){
-    double result = 0;
-    int length = 0;
-
-    while (n!=0){
-        int last_digit = n % 10;
-        if (last_digit == -1){
-            break;
-        }
-        result += last_digit;
-        length++;
-        n/=10;
+void average(int number){
+    double sum = 0;
+    int len = 0;
+    while (number!=0){
+        int last_digit = number%10;
+        sum += last_digit;
+        len++;
+        number/=10;
     }
-    result /= length;
-    return result;
+    printf("Average: %.2f\n", sum/len);
 }
 
 int main(){
     int number_of_lines;
+    int n=0;
     printf("Enter number of lines: ");
     scanf("%d", &number_of_lines);
 
-    int init = number_of_lines;
-    int tracker = 1;
-    while (init >= 0){
-        int user_input;
-        printf("Enter line %d (end with -1): ", tracker);
-        scanf("%d", &user_input);
 
-        printf("%.2f\n",averager(user_input));
-        tracker++;
-        init--;
+    while (number_of_lines!=0){
+        unsigned int line; // reads upto (excluding) negative numbers
+        int cache;
+        printf("Enter line %d (end with -1): ",n); 
+        scanf("%d", &line);
+        scanf("%d",&cache); // catches unwanted -1
+        average(line);
+        number_of_lines--;
+        n++;
     }
+    return 0;
 }
-
