b2d0d762152cf174d138ec70708fc7868c557b67
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-12 10:56:34 +0800

    Exemplifying the use of call by value and reference

---
+++ Concepts/call-by-value-reference.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void function3(int h, int *k)
+{
+    *k = *k-h;
+    printf("%d %d", h, *k);
+}
+
+void function2(int c, int *d)
+{
+    *d = *d * c;
+    function3(c, d);
+}
+
+void function1(int a, int *b)
+{
+    *b = *b + a;
+    function2(a, b);
+}
+
+void main()
+{
+    int x, y;
+    x = 5;
+    y = 5;
+    function1(x,&y);
+    return 0;
+}
+
+// param a, c & h are passed into the function via call by value
+// param b, d & k are passed into the function via call by reference (pointers and address) - addresses are passed into the function instead of values
