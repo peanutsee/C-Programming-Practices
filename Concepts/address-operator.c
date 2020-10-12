70d15ba7f1dd8359946effca6af92f95baf72965
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-12 10:23:47 +0800

    & operator and variables

---
+++ Concepts/address-operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    int num = 5; // num (variable) stores 5 (value)
+    printf("num = %d\n", num);
+
+    // Address Operator
+    printf("&num = %p\n", &num); // prints the memory address of num (variable) with 5 (value)
+}
