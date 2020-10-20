d12e629ca39c598101ff5b7d201a162e7f51dcd3
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-20 18:15:46 +0800

    Create EX2 - printAverage.c

---
+++ CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 2/EX2 - printAverage.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int total, count, lines, input;
+    double average;
+    int i;
+
+    printf("Enter number of lines: \n");
+    scanf("%d", &lines);
+
+    /* Write your program code here */
+    for (i=0; i<lines; i++)
+    {
+        total = 0;
+        count = 0;
+        printf("Enter line %d (end with -1): \n", i+1);
+        scanf("%d", &input);
+        while (input != -1) // scan until -1
+        {
+            total += input;
+            count++;
+            scanf("%d", &input);
+        }
+        average = (double)total/(double)count;
+        printf("Average = %0.2f\n", average);
+    }
+    return 0;
+}
