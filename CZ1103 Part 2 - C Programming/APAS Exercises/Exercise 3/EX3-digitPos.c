d8ed9e3e5c8c722f9093fbbba11672d88b075ab9
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-22 09:39:33 +0800

    Create EX3-digitPos.c

---
+++ CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 3/EX3-digitPos.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int digitPos1(int num, int digit);
+void digitPos2(int num, int digit, int *result);
+int main()
+{
+    int number, digit, result=0;
+
+    printf("Enter the number: \n");
+    scanf("%d", &number);
+    printf("Enter the digit: \n");
+    scanf("%d", &digit);
+    printf("digitPos1(): %d\n", digitPos1(number, digit));
+    digitPos2(number, digit, &result);
+    printf("digitPos2(): %d\n", result);
+    return 0;
+}
+int digitPos1(int num, int digit)
+{
+    /* Write your program code here */
+    int i = 1;
+    while (num!=0)
+    {
+        int last_digit = num % 10;
+        if (last_digit == digit)
+        {
+            return i;
+        }
+        i++;
+        num /= 10;
+    }
+    return 0;
+}
+void digitPos2(int num, int digit, int *result)
+{
+    /* Write your program code here */
+    *result = 1;
+    while (num!=0)
+    {
+        int last_digit = num % 10;
+        if (last_digit == digit)
+        {
+            return result;
+        }
+        (*result)++;
+        num /= 10;
+    }
+    *result = 0;
+    return *result;
+
+}
