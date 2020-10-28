b5586e45ea14b4e79b1d1fa6331b3e5585c326f3
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-10-28 16:34:23 +0800

    Create strncpy.c

---
+++ CZ1103 Part 2 - C Programming/Week 11/strncpy.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define INIT_VALUE 999
+
+char *strncpy(char *s1, char *s2, int n);
+
+int main()
+{
+    char s1[INIT_VALUE], *s2 = "Darryl";
+    strncpy(s1, s2, 7);
+    return 0;
+}
+
+char *strncpy(char *s1, char *s2, int n)
+{
+    int i=0;
+    int len = strlen(s2);
+    for (i; s2[i] != '\0' && i<n; i++)
+    {
+        s1[i] = s2[i];
+    }
+
+    if (n > len)
+    {
+        while (len < n)
+        {
+            s1[len] = '\0';
+            len++;
+        }
+    }
+    s1[len] = '\0';
+    puts(s1);
+    return 0;
+}
