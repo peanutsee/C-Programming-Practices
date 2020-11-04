b5378e1a9ad0723b680e759fd0e636e88d8b5b2f
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-04 10:35:34 +0800

    can i remove fflsuh?

--- CZ1103 Part 2 - C Programming/Week 12/phonebk.c
+++ CZ1103 Part 2 - C Programming/Week 12/phonebk.c
@@ -52,14 +52,47 @@ int main()
 void printPB(PhoneBk *pb, int size)
 {
     /* Write your code here */
+    for (int i=0; i<size; i++)
+    {
+        printf("Name: %sTelno: %d\n", pb[i].name, pb[i].telno);
+    }
 }
 
 int readin(PhoneBk *pb)
 {
     /* Write your code here */
+    int size = 0;
+    while (1)
+    {
+        printf("Enter name:\n");
+        fgets(pb[size].name, 20, stdin);
+        if (strchr(pb[size].name, '#')) break;
+        printf("Enter tel:\n");
+        scanf("%d", &pb[size].telno);
+        fflush(stdin);
+        size++;
+    }
+    return size;
 }
 
 void search(PhoneBk *pb, int size, char *target)
 {
     /* Write your code here */
+    if (size == 0)
+    {
+        printf("Empty phonebook\n");
+    }
+    else
+    {
+        for (int i=0; i<size; i++)
+        {
+            if (strcmp(pb[i].name, target))
+            {
+                printf("Name= %sTel= %d\n", pb[i].name, pb[i].telno);
+                break;
+            }
+        }
+        printf("Name not found!\n");
+    }
+
 }
