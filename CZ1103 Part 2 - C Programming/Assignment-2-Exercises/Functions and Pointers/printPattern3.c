4ce4527aa95d0cf215d4621c481436a0e5bd1b15
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-09 03:17:49 +0800

    Create printPattern3.c

---
+++ CZ1103 Part 2 - C Programming/Assignment-2-Exercises/Functions and Pointers/printPattern3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printPattern2(int height);
+
+int main()
+{
+    int height;
+    printf("Enter the height: \n");
+    scanf("%d", &height);
+    printf("printPattern2(): \n");
+    printPattern2(height);
+    return 0;
+}
+
+void printPattern2(int height)
+{
+    /* Write your code here */
+    for (int i=1; i<=height; i++)
+    {
+        for (int j=0; j<i; j++)
+        {
+            printf("%d ", (i+j) % 10);
+        }
+        puts("");
+    }
+}
