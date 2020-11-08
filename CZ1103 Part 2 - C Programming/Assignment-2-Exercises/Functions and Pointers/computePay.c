ef3ce1bd818b52758a430035c8483d457f63aedf
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-08 14:53:12 +0800

    Update computePay.c

--- CZ1103 Part 2 - C Programming/Assignment-2-Exercises/Functions and Pointers/computePay.c
+++ CZ1103 Part 2 - C Programming/Assignment-2-Exercises/Functions and Pointers/computePay.c
@@ -23,7 +23,7 @@ double computePay1(int noOfHours, int payRate)
     double pay;
     if (noOfHours > 160)
     {
-        pay = ((noOfHours - 160) * payRate * 1.5) + 160*6;
+        pay = ((noOfHours - 160) * payRate * 1.5) + 160*payRate;
     }
     else
     {
@@ -38,7 +38,7 @@ void computePay2(int noOfHours, int payRate, double *grossPay)
     (*grossPay) = 0;
     if (noOfHours > 160)
     {
-        (*grossPay) = ((noOfHours - 160) * payRate * 1.5) + 160*6;
+        (*grossPay) = ((noOfHours - 160) * payRate * 1.5) + 160*payRate;
     }
     else
     {
