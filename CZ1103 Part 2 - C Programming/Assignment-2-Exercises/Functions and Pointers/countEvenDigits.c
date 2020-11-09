9611aad5feebbc5368a3feb289bc7466d4cdec81
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-09 11:31:05 +0800

    Create countEvenDigits.c

---
+++ CZ1103 Part 2 - C Programming/Assignment-2-Exercises/Functions and Pointers/countEvenDigits.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int countEvenDigits1(int number);
+void countEvenDigits2(int number, int *count);
+
+int main()
+{
+    int number, result;
+    printf("Enter a number: \n");
+    scanf("%d", &number);
+    printf("countEvenDigits1(): %d\n", countEvenDigits1(number));
+    countEvenDigits2(number, &result);
+    printf("countEvenDigits2(): %d\n", result);
+    return 0;
+}
+
+int countEvenDigits1(int number)
+{
+    /* Write your code here */
+    int count = 0;
+
+    do
+    {
+        int last_digit = number % 10;
+        if (last_digit % 2 == 0)
+        {
+            count++;
+        }
+        number /= 10;
+    } while (number != 0);
+    return count;
+}
+
+void countEvenDigits2(int number, int *count)
+{
+    /* Write your code here */
+    *count = 0;
+
+    do
+    {
+        int last_digit = number % 10;
+        if (last_digit % 2 == 0)
+        {
+            (*count)++;
+        }
+        number /= 10;
+    } while (number != 0);
+}
