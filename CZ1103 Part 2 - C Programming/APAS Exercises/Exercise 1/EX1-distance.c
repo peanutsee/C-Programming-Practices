26b14cc5704a5d7cc547786e64b8fa0c3703b1f4
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-14 11:54:36 +0800

    Update EX1-distance.c

--- CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 1/EX1-distance.c
+++ CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 1/EX1-distance.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-
-int main()
+int main() 
 {
-    /* Write your program code here */
-    int x1, y1, x2, y2;
-    printf("Enter first point x1 y1:\n");
-    scanf("%d", &x1);
-    scanf("%d", &y1);
-
-    printf("Enter second point x2 y2:\n");
-    scanf("%d", &x2);
-    scanf("%d", &y2);
-
-    double result, lhs, rhs;
-    lhs = pow((double)(x2-x1), 2);
-    rhs = pow((double)(y2-y1), 2);
-    result = sqrt(lhs + rhs);
-    printf("The distance is %0.2f", result);
-
-    return 0;
+   /* Write your program code here */
+   /// sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+   double x1, x2, y1, y2, distance;
+   
+   printf("Enter first point x1 y1:\n");
+   scanf("%lf %lf", &x1, &y1);
+   printf("Enter second point x2 y2:\n");
+   scanf("%lf %lf", &x2, &y2);
+   
+   distance = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+   printf("The distance is %.2f\n", distance);
+   
+   return 0;
 }
