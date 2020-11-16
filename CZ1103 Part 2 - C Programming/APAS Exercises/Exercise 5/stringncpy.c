d35595ca94988e63163369b6e8e75a2d0360cce7
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-17 01:23:14 +0800

    Refactored and adapted

--- CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 5/stringncpy.c
+++ CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 5/stringncpy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 char *stringncpy(char *s1, char *s2, int n);
 int main()
 {
@@ -12,14 +13,22 @@ int main()
    printf("stringncpy(): %s\n", target);
    return 0;
 }
+
 char *stringncpy(char *s1, char *s2, int n)
 {
    /* Write your program code here */
-    int i=0;
-    for (i; i<n; i++)
-    {
-        s1[i] = s2[i];
-    }
-    s1[i] = '\0';
-    return s1;
+   int i=0;
+   for (i; i<n; i++)
+   {
+       //printf("i: %d\n", i);
+       if (isalpha(s2[i]))
+       {
+           //printf("%c\n", s2[i]);
+           s1[i] = s2[i];
+       }
+           
+       else s1[i] = '*'; // Appends custom character to EOF
+   }
+   s1[n] = '\0'; // NULL character at EOF
+   return s1;
 }
