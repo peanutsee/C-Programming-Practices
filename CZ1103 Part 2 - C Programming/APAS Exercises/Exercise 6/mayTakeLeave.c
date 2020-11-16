505522f9c740146c1707746491928bd6912cb37e
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-17 00:30:34 +0800

    Update mayTakeLeave.c

--- CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 6/mayTakeLeave.c
+++ CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 6/mayTakeLeave.c
@@ -1,4 +1,3 @@
-  
 #include <stdio.h>
 #define INIT_VALUE 1000
 typedef struct {
@@ -15,74 +14,88 @@ int main()
    int len;
    int id, leave, canTake=INIT_VALUE;
    int choice;
-
+   
    printf("Select one of the following options: \n");
-   printf("1: getInput()\n");
+   printf("1: getInput()\n");     
    printf("2: printList()\n");
    printf("3: mayTakeLeave()\n");
-   printf("4: exit()\n");
+   printf("4: exit()\n"); 
    do {
       printf("Enter your choice: \n");
       scanf("%d", &choice);
       switch (choice) {
-         case 1:
+         case 1:  
             getInput(listRec, &len);
             printList(listRec, len);
             break;
-         case 2:
+         case 2:  
             printList(listRec, len);
             break;
-         case 3:
+         case 3:  
             printf("Please input id, leave to be taken: \n");
             scanf("%d %d", &id, &leave);
             canTake = mayTakeLeave(listRec, id, leave, len);
             if (canTake == 1)
                printf("The staff %d can take leave\n", id);
             else if (canTake == 0)
-               printf("The staff %d cannot take leave\n", id);
+               printf("The staff %d cannot take leave\n", id); 
             else if (canTake == -1)
-               printf("The staff %d is not in the list\n", id);
+               printf("The staff %d is not in the list\n", id); 
             else
                printf("Error!");
-            break;
+            break;     
       }
-   } while (choice < 4);
+   } while (choice < 4);    
    return 0;
 }
-
 void printList(leaveRecord list[], int n)
 {
-    int p;
+   int p;
 
-    printf("The staff list:\n");
-    for (p = 0; p < n; p++)
-        printf ("id = %d, totalleave = %d, leave taken = %d\n",
-            list[p].id, list[p].totalLeave, list[p].leaveTaken);
+   printf("The staff list:\n");
+   for (p = 0; p < n; p++)
+      printf ("id = %d, totalleave = %d, leave taken = %d\n", 
+         list[p].id, list[p].totalLeave, list[p].leaveTaken);
 }
+
 void getInput(leaveRecord list[], int *n)
 {
 	/* Write your program code here */
-	printf("Enter the number of staff records: \n");
+	/*
+	Each line of the input has three integers representing one staff identifier, 
+	his/her total number of days of leave allowed and his/her number of days of leave taken so far respectively. 
+	The function will read the data into the array list until end of input and returns the number of records read through n.
+	*/
+	printf("Enter the number of staff records:\n");
 	scanf("%d", n);
-
-	for (int i=0; i<*n; i++)
+	
+	int i=0;
+	for (i; i<*n; i++)
 	{
-	    printf("Enter id, totalleave, leavetaken: \n");
+	    printf("Enter id, totalleave, leavetaken:\n");
 	    scanf("%d %d %d", &list[i].id, &list[i].totalLeave, &list[i].leaveTaken);
 	}
-}
+} 
 
 int mayTakeLeave(leaveRecord list[], int id, int leave, int n)
 {
 	/* Write your program code here */
-	// Check if ID is in list
-	for (int i=0; i<n; i++)
+	/*
+	It returns 1 if a leave application for leave days is approved. 
+	Staff member with identifier id is applying for leave days of leave. n is the number of staff in list. 
+	Approval will be given if the leave taken so far plus the number of days applied for is less than or equal to 
+	his total number of leave days allowed. If approval is not given, it returns 0. 
+	It will return ‑1 if no one in list has identifier id.
+	*/
+	int i=0, found = 0;
+	for (i; i<n; i++)
 	{
 	    if (list[i].id == id)
 	    {
-	        int total_leaves_taken = list[i].leaveTaken + leave;
-            return list[i].totalLeave >= total_leaves_taken ? 1:0;
+	        found = 1;
+	        return list[i].totalLeave >= list[i].leaveTaken + leave? 1:0;
 	    }
 	}
-	return -1;
+	if (found==0) return -1;
+	
 }
