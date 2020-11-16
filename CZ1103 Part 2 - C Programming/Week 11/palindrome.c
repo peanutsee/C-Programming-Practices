863f8bced425d91192f847de59f286375f03a9df
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-16 15:30:48 +0800

    Update palindrome.c

--- CZ1103 Part 2 - C Programming/Week 11/palindrome.c
+++ CZ1103 Part 2 - C Programming/Week 11/palindrome.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-#define INIT_VALUE -1000
+#define INIT_VALUE -1
+
 int palindrome(char *str);
+
 int main()
 {
-    char str[80], *p;
-    int result = INIT_VALUE;
-    printf("Enter a string: \n");
-    fgets(str, 80, stdin);
-    if (p=strchr(str,'\n')) *p = '\0';
-    result = palindrome(str);
-    if (result == 1)
-        printf("palindrome(): A palindrome\n");
-    else if (result == 0)
-        printf("palindrome(): Not a palindrome\n");
-    else
-        printf("An error\n");
-    return 0;
+   char str[80];
+   int result = INIT_VALUE;
+   
+   printf("Enter a string: \n");
+   gets(str);
+   result = palindrome(str);
+   if (result == 1)
+      printf("palindrome(): A palindrome\n");
+   else if (result == 0)
+      printf("palindrome(): Not a palindrome\n");    
+   else 
+      printf("An error\n");      
+   return 0;
 }
 
 int palindrome(char *str)
 {
-    /* Write your code here */
-    char *rev_str;
-    int tracker = 0;
-    for (int i=strlen(str)-1; i>-1; i--) // Remember last character is \0 in strings
-    {
-        rev_str[tracker] = str[i];
-        tracker++;
-    }
-    int result = strcmp(str, rev_str) == 0? 1: 0;
-    return result;
+   /* Write your code here */
+   int len = strlen(str), i=0, j;
+   char palindrome[len];
+   
+   for (j=len-1; j>-1; j--)
+   {
+       palindrome[i] = str[j];
+       i++;
+   }
+   return strcmp(palindrome, str) == 0? 1:0;
 }
