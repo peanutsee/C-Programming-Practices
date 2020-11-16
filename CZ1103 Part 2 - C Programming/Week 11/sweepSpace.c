48a21d13ac57f45be530741a29cf3404bd2a7839
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-16 14:22:04 +0800

    Update sweepSpace.c

--- CZ1103 Part 2 - C Programming/Week 11/sweepSpace.c
+++ CZ1103 Part 2 - C Programming/Week 11/sweepSpace.c
@@ -1,49 +1,67 @@
 #include <stdio.h>
-#include <string.h>
-
-char *sweepSpace1(char *str);
-char *sweepSpace2(char *str);
-
+char *sweepSpace1(char *sentence);
+char *sweepSpace2(char *sentence);
 int main()
 {
-    char str[80],str2[80], *p;
-    printf("Enter the string: \n");
-    fgets(str, 80, stdin);
-    if (p=strchr(str,'\n')) *p = '\0';
-    strcpy(str2,str);
-    printf("sweepSpace1(): %s\n", sweepSpace1(str));
-    printf("sweepSpace2(): %s\n", sweepSpace2(str2));
-    return 0;
+   char str[80];
+ 
+   printf("Enter the string: \n");
+   gets(str);   
+   printf("sweepSpace1(): %s\n", sweepSpace1(str));
+   printf("sweepSpace2(): %s\n", sweepSpace2(str));
+   return 0;
 }
 
-char *sweepSpace1(char *str)
+char *sweepSpace1(char *sentence)  
 {
-    /* Write your program code here */
-    int k = 0;
-    for (int i=0; str[i]; i++)
-    {
-        str[i] = str[i+k];
-        if (str[i] == ' ')
-        {
-            k++;
-            i--;
+   int i, j, len;
+   
+   i=0; len=0;
+   while (sentence[i]!='\0'){
+      len++;
+      i++;
+   }
+   j = 0;
+   for (i=0; i < len; i++)
+   {
+      	if (sentence[i] == ' ')
+      	{
+             // update the string by removing any space detected
+             for (j=i; j<len-1; j++)
+             {
+                 sentence[j] = sentence[j+1];
+             }
+             i--;
+            sentence[j] = '\0'; // add a null character 
         }
     }
-    return str;
+    return sentence;
 }
 
-char *sweepSpace2(char *str)
+char *sweepSpace2(char *sentence)  
 {
-    /* Write your program code here */
-    int k = 0;
-    for (int i=0; str[i]; i++)
-    {
-        *(str+i) = *(str+i+k);
-        if (str[i] == ' ')
-        {
-            k++;
-            i--;
+   /* Write your program code here */
+   int i, j, len;
+   
+   i=0; len=0;
+   while (*(sentence+i)!='\0'){
+      len++;
+      i++;
+   }
+   j = 0;
+   for (i=0; i < len; i++)
+   {
+      	if (*(sentence+i) == ' ')
+      	{
+             // update the string by removing any space detected
+             for (j=i; j<len-1; j++)
+             {
+                 *(sentence+j) = *(sentence+j+1);
+             }
+             i--;
+             *(sentence+j) = '\0'; // add a null character 
         }
     }
-    return str;
+    
+    return sentence;
 }
