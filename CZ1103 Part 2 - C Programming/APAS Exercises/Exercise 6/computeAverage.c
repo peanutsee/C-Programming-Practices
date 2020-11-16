8fc7a93a88d6431d1121b71d2626b4164e27f704
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-17 00:16:07 +0800

    Update computeAverage.c

--- CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 6/computeAverage.c
+++ CZ1103 Part 2 - C Programming/APAS Exercises/Exercise 6/computeAverage.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+
 struct student{
    char name[20]; /* student name */
    double testScore; /* test score */
@@ -8,49 +9,37 @@ struct student{
 };
 
 double average();
-
 int main()
 {
-   printf("average(): %.2f\n", average());
+   printf("average(): %.2f\n", average());           
    return 0;
 }
-
 double average()
 {
    /* Write your program code here */
-    char *p;
-    struct student array[50];
-    int tracker = 0;
-
-    while (1)
-    {
-        scanf("\n");
-        printf("Enter student name:\n");
-        fgets(array[tracker].name, 20, stdin);
-        // Termination condition
-        if (strcmp(array[tracker].name, "END\n") == 0) break;
-        if (p=strchr(array[tracker].name, '\n')) *p = '\0';
-
-        
-
-        printf("Enter test score:\n");
-        scanf("%lf", &array[tracker].testScore);
-
-        printf("Enter exam score:\n");
-        scanf("%lf", &array[tracker].examScore);
-
-        array[tracker].total = (array[tracker].testScore + array[tracker].examScore)/2;
-        printf("Student %s total = %.2f\n", array[tracker].name, array[tracker].total);
-        tracker++;
-    }
-
-    double total;
-
-    for (int i=0; i<=tracker; i++)
-    {
-        total += array[i].total;
-    }
-
-    double average = total/tracker;
-    return tracker != 0? average:0.00;
+   double globalTotal, count=0;
+   struct student s;
+   char *p;
+   
+   while (1)
+   {
+       printf("Enter student name:\n");
+       scanf("\n");
+       fgets(s.name, 20, stdin);
+       if (p=strchr(s.name, '\n')) *p='\0';
+       if (strcmp(s.name, "END")==0) break;
+       
+       printf("Enter test score:\n");
+       scanf("%lf", &s.testScore);
+       
+       printf("Enter exam score:\n");
+       scanf("%lf", &s.examScore);
+       
+       s.total = (s.testScore + s.examScore) / 2;
+       printf("Student %s total = %.2f\n", s.name, s.total);
+       
+       globalTotal += s.total;
+       count++;
+   }
+   return count==0?0:globalTotal/count;
 }
