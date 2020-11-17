15f6032ebb64dd05d0350b949a250b586bb440d2
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2020-11-17 11:41:53 +0800

    Update platform1D.c

--- CZ1103 Part 2 - C Programming/Assignment-2-Exercises/Arrays/platform1D.c
+++ CZ1103 Part 2 - C Programming/Assignment-2-Exercises/Arrays/platform1D.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 int platform1D(int ar[], int size);
-
 int main()
 {
     int i,b[50],size;
@@ -13,26 +11,24 @@ int main()
     printf("platform1D(): %d\n", platform1D(b,size));
     return 0;
 }
-
 int platform1D(int ar[], int size)
 {
     /* Write your code here */
     int temp_count=0, max_count=0, current=ar[0];
-
     for (int i=0; i<size; i++)
     {
-        if (ar[i] == current)
+        if (ar[i] == current) // Checks to see if it match current 
         {
             temp_count++;
-            if (i==size-1)
+            if (i==size-1) 
             {
-                max_count = temp_count;
+                max_count = temp_count; // Handles cases like 1 2 2 2 2 (last element no change)
             }
         }
         else 
         {
-            if (temp_count > max_count) max_count = temp_count;
-            current = ar[i];
+            if (temp_count > max_count) max_count = temp_count; // Updates max_count
+            current = ar[i]; // Updates new current
         }
     }
     return max_count;
